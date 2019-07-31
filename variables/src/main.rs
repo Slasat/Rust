7dee96661df07a6e0a77d9032e3cf8db5845c8ec
Vladislav Tikhonov <vladislav.v.tikhonov@gmail.com>
2019-08-01 00:08:01 +0300

    Wrote some code from 'variables' chapter

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    let x: f64 =  x as f64 * 2.0;
+    println!("The value of x is: {}", x/3.5);
+
+    let tup = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
 }
