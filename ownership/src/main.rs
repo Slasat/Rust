244b70b40983eba218dda659295789eca47a1790
LemalPLS <vladislav.v.tikhonov@gmail.com>
2019-08-03 23:20:58 +0300

    Stoped before borrowing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello");
+    s.push_str(", world!");
+    let te = "te";
+    let st = "st";
+    let test = format!("{}{}", te, st);
+    take_ownership(&test);
+    println!("{}", test);
+}
+
+fn take_ownership(s: &String){
+    println!("{}", s);
 }
