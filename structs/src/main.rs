a2a14cebab55092d67d7efbd3ab6d8e54634f21c
LemalPLS <vladislav.v.tikhonov@gmail.com>
2019-08-04 17:25:44 +0300

    End first struct chapter

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,34 @@
+struct User{
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someoneusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+    let user2 = build_user(String::from("someonetwo@gmail.com"), String::from("someonetwousername"));
+    let user3 = User {
+        email: String::from("someonethree@exmaple.com"),
+        username: String::from("someonethreeusername"),
+        ..user2
+    };
+
+
+
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
