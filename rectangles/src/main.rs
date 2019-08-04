a8b43a8f606083181c8af195da1df522e43e0c9f
LemalPLS <vladislav.v.tikhonov@gmail.com>
2019-08-04 18:06:32 +0300

    Ended struct chapter

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height:u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let rect1 = Rectangle { width :30, height: 50};
+
+    println!("The area of rectangle {:?} is {} square pixels!", rect1, rect1.area());
 }
