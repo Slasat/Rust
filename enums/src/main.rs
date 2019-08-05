783f1c532ccf322b2a86cdcb4ea5fa105c3c4ce4
LemalPLS <vladislav.v.tikhonov@gmail.com>
2019-08-05 23:10:37 +0300

    Ended on match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String, 
+} 
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+     
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+
+    let loopback = IpAddrKind::V6(String::from("::1"));
+}
+
+fn route(ip_kind: IpAddrKind) {}
